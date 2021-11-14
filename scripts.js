60df708202ad2045934f606998a6cfcb7e66c54b
Christina Milner <duckodile@gmail.com>
2021-11-14 16:37:43 +0000

    Logic fully implemented

--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
 // ComputerPlay function
-// We haven't learned about arrays yet but no idea how the hell else you would do this
 
 const choices = ["rock", "paper", "scissors"]
-
-// helper function to get a random integer between 1 and 3 I've nicked off ws3schools
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1 )) + min;
@@ -13,27 +10,25 @@ function computerPlay() {
     let index = getRndInteger(1, 3) - 1;
     return choices[index];
 }
-/*
-function getPlayerChoice() {
-    console.log("Let's play a game of rock, paper, scissors!");
-    let playerChoice = prompt("Please choose rock, paper, or scissors.");
-    // for extra credit I'm gonna check for garbage input here because first thing I would do
-    // is enter a curse word and see what happens
-    if (choices.indexOf(playerChoice.toLowerCase()) < 0) {
-        getPlayerChoice();
-        // yay for recursion so I don't need to figure out while loops in this ahead of time
-    }
-    else {
-    return(playerChoice.toLowerCase());
-    }
-}
-*/
+
+// Query selectors and variables
+
+let playerScore = 0;
+let computerScore = 0;
+
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         playRPS(button.className);
     });
 });
+
+const resultText = document.querySelector('.result');
+const scoreText = document.querySelector('.score');
+
+
+// functions
+// balls, I'm going to have to deconstruct game() and stuff it into the other functions now that there's no longer a loop
 
 function getWinner(choiceA, choiceB) {
     if (choiceA === choiceB) {
@@ -53,17 +48,17 @@ function getWinner(choiceA, choiceB) {
 function declareWinner(playerChoice, computerChoice) {
     let result = getWinner(playerChoice, computerChoice);
     if (!result) {
-        console.log("It's a draw!");
+        resultText.textContent = "It's a draw!";
         return 0;
     }
     else if (result === playerChoice) {
-        console.log(`You win - ${playerChoice} beats ${computerChoice}!`);
+        resultText.textContent = `You win - ${playerChoice} beats ${computerChoice}!`;
         // can't have exclamation mark after 'You win' or lowercase variables become grammatically incorrect
         // there is a method for converting to title case on StackOverflow but this seems much easier
         return "player";
     }
     else {
-        console.log(`You lose - ${computerChoice} beats ${playerChoice}!`);
+        resultText.textContent = `You lose - ${computerChoice} beats ${playerChoice}!`;
         return "computer";
     }
 }
@@ -71,39 +66,13 @@ function declareWinner(playerChoice, computerChoice) {
 function playRPS(buttonInput) {
     let playerChoice = buttonInput;
     let computerChoice = computerPlay();
-    return declareWinner(playerChoice, computerChoice);
-}
-
-/*
-Game function
-Needs to do a for loop to run playRPS 5 times
-Needs to also keep score... how to do this?
-"Feel free to re-work your previous functions if you need to. Specifically, you might
-want to change the return value to something more useful."
-Ye, let's change the returns in declareWinner to console.logs and make it return who won instead
-Derr, also have to return declareWinner in the above function or none of this works
-*/
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-        let round = playRPS();
-        if (round === "player") {
-            playerScore += 1;
-        }
-        else if (round === "computer") {
-            computerScore +=1;
-        }
-    
-    if (playerScore > computerScore) {
-        console.log(`You win ${playerScore}:${computerScore} vs the computer.`)
+    let round = declareWinner(playerChoice, computerChoice);
+    if (round === "player") {
+        playerScore += 1;
     }
-    else if (computerScore > playerScore) {
-        console.log(`You lose! The computer wins ${computerScore}:${playerScore}.`)
+    else if (round === "computer") {
+        computerScore +=1;
     }
-    else {
-        console.log('Aw, after 5 rounds, it\'s still a draw!')
-        //Forgot a draw after 5 rounds is still possible when some of the rounds are a draw...
-    }
+    scoreText.textContent = `Player: ${playerScore} - Computer: ${computerScore}`
 }
 
